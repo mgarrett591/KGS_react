298a574f449595212ec4e9d892e56fc6f01b32a3
Mark Garrett <mgarrett591@gmail.com>
2021-06-19 17:56:40 -0500

    Copy KGS_CLS.exe to pyKGS and use a relative path

--- pyKGS/pyKGS.py
+++ pyKGS/pyKGS.py
@@ -1,7 +1,7 @@
 import subprocess;
 class pyKGS():
     def Eval(templet, VarTabel):
-        args=['C:\\Users\\mgarrett\\MyRepos\\Python\\KGS\\KGS_CLI\\bin\\Release\\win-x86\\KGS_CLI.exe', templet]
+        args=['KGS_CLI.exe', templet]
         for Key in VarTabel.keys():
             args.append(Key)
             args.append(VarTabel[Key])
