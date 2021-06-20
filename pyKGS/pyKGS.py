f4b1331ff9c5d9ac0c52dff8b31393197a63bf0f
Mark Garrett <mgarrett591@gmail.com>
2021-06-20 01:39:22 -0500

    Remove dotnet KGS_CLI.exe and pyKGS.py

--- pyKGS/pyKGS.py
+++
@@ -1,8 +0,0 @@
-import subprocess;
-class pyKGS():
-    def Eval(templet, VarTabel):
-        args=['KGS_CLI.exe', templet]
-        for Key in VarTabel.keys():
-            args.append(Key)
-            args.append(VarTabel[Key])
-        return subprocess.run(args, stdout=subprocess.PIPE, text=True).stdout.strip()
