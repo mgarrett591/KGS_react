e9429443b725ef9ef24e6ca889e30147224d9ba2
Mark Garrett <mgarrett591@gmail.com>
2021-07-07 00:05:31 -0500

    add interpolator.ts

---
+++ KGS.ts/interpolatortest.ts
@@ -0,0 +1,13 @@
+import { KGSi } from "./KGSi";
+
+let VTable: string[] = [];
+VTable["dog"] = "DOGs";
+console.log(KGSi.Interpolator("",VTable));
+////Miss matched {
+console.log(KGSi.Interpolator("{}{}[]{}[]{}}{}{}[]",VTable));  
+
+//Miss matched [
+console.log(KGSi.Interpolator("{}{}[]{}[{}{}{}{}[]",VTable));
+
+console.log(KGSi.Interpolator("{dog}[]",VTable));
+console.log(KGSi.Interpolator("{dog}[rule]",VTable));
