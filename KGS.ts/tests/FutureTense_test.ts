886232225729ab332adbc4c63809cee149c8f9d8
Mark Garrett <mgarrett591@gmail.com>
2021-07-07 00:21:06 -0500

    add Python version of the unit tests to the TypeScript project, rename the files, change the line endings

---
+++ KGS.ts/tests/FutureTense_test.ts
@@ -0,0 +1,7 @@
+﻿import pytest
+import testCall
+
+Templet = "{var.stem}[ㄹ/을] 거에요"
+
+#def test_TestMethod1():
+#    assert 
---
+++ KGS.ts/tests/StemFCS_test.ts
@@ -0,0 +1,56 @@
+import pytest
+import testCall
+
+FCSTemplet = "{var.fcs}"
+StemTemplet = "{var.stem}"
+
+def test_켜다_Error():
+    assert "켜어" == testCall.Call(FCSTemplet, "켜다")
+
+def  test_하():
+    assert "해" == testCall.Call(FCSTemplet, "하다")
+
+def  test_ㅏ_batchim():
+    assert "작아" == testCall.Call(FCSTemplet, "작다")
+
+def  test_ㅏ_no_batchim():
+    assert "싸" == testCall.Call(FCSTemplet, "싸다")
+
+def  test_ㅗ_no_batchim():
+    assert "와" == testCall.Call(FCSTemplet, "오다")
+
+def  test_ㅣ_batchim():
+    assert "믿어" == testCall.Call(FCSTemplet, "믿다")
+
+def  test_ㅣ_no_batchim():
+    assert "마셔" == testCall.Call(FCSTemplet, "마시다")
+
+def  test_ㅓㅐㅔ_no_batchim():
+    assert "필요없어" == testCall.Call(FCSTemplet, "필요없다")
+
+def  test_ㅓㅐㅔ_batchim():
+    assert "끝내" == testCall.Call(FCSTemplet, "끝내다")
+
+def  test_ㅜ_batchim():
+    assert "높아" == testCall.Call(FCSTemplet, "높다")
+
+def  test_ㅜ_no_batchim():
+    assert "줘" == testCall.Call(FCSTemplet, "주다")
+
+def  test_ㅡ_batchim():
+    assert "떠들어" == testCall.Call(FCSTemplet, "떠들다")
+
+def  test_ㅡ_no_batchim_no_ㅗorㅏ(): #May need redefine OhAh..
+    assert "나빠" == testCall.Call(FCSTemplet, "나쁘다")
+
+def  test_ㅡ_no_batchim_ㅗorㅏ(): #May need redefine OhAh..
+    assert "잠가" == testCall.Call(FCSTemplet, "잠그다")
+
+def  test_ㅟㅚ():
+    assert "사귀어" == testCall.Call(FCSTemplet, "사귀다")
+
+def  test_NonVerb():
+    assert "?" == testCall.Call(FCSTemplet, "Dict")
+
+def  test_Da():
+    assert "?" == testCall.Call(FCSTemplet, "다")
