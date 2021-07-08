9b255a696d009fe4b75dcc72703474eba4cb69cb
Mark Garrett <mgarrett591@gmail.com>
2021-07-07 21:06:53 -0500

    Remove non dotnet code

--- KGS.ts/testcall.ts
+++
@@ -1,6 +0,0 @@
-import { KGSi } from "./KGSi";
-export class TestCall{
-    public Call(Templet: string, Var: string){
-        return KGSi.Interpolator(Templet, [Var])
-    };
-};
--- Python/pytest_PastTense_test.py
+++
@@ -1,7 +0,0 @@
-﻿import pytest
-import testCall
-
-Templet = "{var.fcs}[ㅆ]어요"
-
-#def test_TestMethod1():
-#    assert
