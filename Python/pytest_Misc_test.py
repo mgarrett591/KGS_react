a55403b415f147358c850c2c5bdcc26eece88cb8
Mark Garrett <mgarrett591@gmail.com>
2021-07-07 21:08:58 -0500

    Remove non python files

--- Python/pytest_Misc_test.py
+++
@@ -1,5 +0,0 @@
-﻿import pytest
-import testCall
-
-def test_KeepCase():
-    assert "Success!" == testCall.Call("{var}", "Success!")
--- pyKGS/pytest_FutureTense_test.py
+++
@@ -1,7 +0,0 @@
-﻿import pytest
-import testCall
-
-Templet = "{var.stem}[ㄹ/을] 거에요"
-
-#def test_TestMethod1():
-#    assert 
