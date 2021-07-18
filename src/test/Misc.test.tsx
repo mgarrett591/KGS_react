16acf461f0f7274100c4d2cb2a281ede7b949319
Mark Garrett <mgarrett591@gmail.com>
2021-07-17 21:06:36 -0500

    Add sytax checking tests

--- src/test/Misc.test.tsx
+++ src/test/Misc.test.tsx
@@ -4,3 +4,33 @@ test("Don't mess with the caseness of the var values'", () => {
     let Value: string = TestCall.Call("{var}", "Success!");
     expect(Value).toEqual("Success!");
 });
+
+test("Sytax Error'", () => {
+    let Value: string = TestCall.Call("[var}", "Success!");
+    expect(Value).toEqual("Check your Syntax");
+});
+
+test("Sytax Error'", () => {
+    let Value: string = TestCall.Call("{var]", "Success!");
+    expect(Value).toEqual("Check your Syntax");
+});
+
+test("Sytax Error'", () => {
+    let Value: string = TestCall.Call("{{var]", "Success!");
+    expect(Value).toEqual("Check your Syntax");
+});
+
+test("Sytax Error'", () => {
+    let Value: string = TestCall.Call("[var}}", "Success!");
+    expect(Value).toEqual("Check your Syntax");
+});
+
+test("Sytax Error'", () => {
+    let Value: string = TestCall.Call("{var]]", "Success!");
+    expect(Value).toEqual("Check your Syntax");
+});
+
+test("Sytax Error'", () => {
+    let Value: string = TestCall.Call("[[var}", "Success!");
+    expect(Value).toEqual("Check your Syntax");
+});
